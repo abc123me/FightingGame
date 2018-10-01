c1527036042edd9bb2ff8abf03a67eb28384cb6d
jeremiah <jeremiah@t430>
2018-10-01 15:15:43 -0400

    Removed updating look positon with position data since thats stupid

--- src/jeremiahlowe/fightinggame/net/struct/PositionData.java
+++ src/jeremiahlowe/fightinggame/net/struct/PositionData.java
@@ -5,19 +5,20 @@ import net.net16.jeremiahlowe.shared.math.Vector;
 
 public class PositionData {
 	public final long forUUID;
-	public final Vector pos, look;
+	public final Vector pos;
+	//public final Vector look;
 	
 	public PositionData(Player pl) {
 		this(pl.pos, pl.look, pl.uuid);
 	}
 	public PositionData(Vector pos, Vector look, long forUUID) {
 		this.pos = pos;
-		this.look = look;
+		//this.look = look;
 		this.forUUID = forUUID;
 	}
 	
 	public void copyTo(Player to) {
 		to.pos = pos;
-		to.look = look;
+		//to.look = look;
 	}
 }
